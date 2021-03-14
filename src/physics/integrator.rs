9a969b8468e4aa56d1202d198dfc1ee071e74f3e
Aart Stuurman <aartstuurman@hotmail.com>
2021-03-14 20:42:11 +0100

    Made integrator continuous

--- src/physics/integrator.rs
+++ src/physics/integrator.rs
@@ -1,17 +1,21 @@
+extern crate nalgebra as na;
 use super::rigidbody::Rigidbody;
+use na::Vector3;
 
 pub struct Integrator {
 	/// Gravitational acceleration in m/s^2
-	gravity: f32,
+	gravity: Vector3<f32>,
 }
 
 impl Integrator {
 	pub fn new(gravity: f32) -> Integrator {
-		return Integrator { gravity: gravity };
+		return Integrator {
+			gravity: Vector3::new(0.0, -gravity, 0.0),
+		};
 	}
 
 	pub fn step(&self, body: &mut Rigidbody, delta: f32) {
-		body.velocity.z -= self.gravity * delta;
-		body.position += body.velocity * delta;
+		body.position = body.position + delta * body.velocity + 0.5 * self.gravity * delta * delta;
+		body.velocity = body.velocity + delta * self.gravity;
 	}
 }

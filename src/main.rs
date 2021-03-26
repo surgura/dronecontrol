20e2a613ddc80d85e3d9f13cf3f179c4b8606fb0
Aart Stuurman <aartstuurman@hotmail.com>
2021-03-26 23:22:01 +0100

    Working simulation for one motor :)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate nalgebra as na;
 mod physics;
-use na::Matrix3;
+use na::{Matrix3, Vector3};
 use physics::integrator::Integrator;
 use physics::rigidbody::Rigidbody;
+use plotters::prelude::*;
 
 fn main() {
     // width = depth = 0.5
@@ -27,9 +28,84 @@ fn main() {
     let integrator = Integrator::new(9.81);
 
     //println!("{:?}", drone);
-    for _ in 0..10 {
+    let mut positions: Vec<Vector3<f32>> = Vec::new();
+    let mut rotations: Vec<Vector3<f32>> = Vec::new();
+    positions.push(drone.position);
+    rotations.push(drone.rotation);
+    for _ in 0..1000 {
         integrator.step(&mut drone, 0.01);
         //println!("{:?} {:?}", drone.rotation, drone.angular_velocity);
-        println!("{:?}", drone.rotation);
+        //println!("{:?}", drone.position);
+        positions.push(drone.position);
+        rotations.push(drone.rotation);
     }
+
+    let root_drawing_area = BitMapBackend::new("images/0.1.png", (1024, 768)).into_drawing_area();
+
+    root_drawing_area.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&root_drawing_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .build_cartesian_2d(0.0..3.14, -8.0..8.0)
+        .unwrap();
+
+    chart.configure_mesh().draw().unwrap();
+
+    chart
+        .draw_series(LineSeries::new(
+            positions
+                .iter()
+                .enumerate()
+                .map(|(i, v)| (i as f64 * 0.01, v[0] as f64)),
+            &RED,
+        ))
+        .unwrap();
+    chart
+        .draw_series(LineSeries::new(
+            positions
+                .iter()
+                .enumerate()
+                .map(|(i, v)| (i as f64 * 0.01, v[1] as f64)),
+            &BLUE,
+        ))
+        .unwrap();
+    /*chart
+    .draw_series(LineSeries::new(
+        positions
+            .iter()
+            .enumerate()
+            .map(|(i, v)| (i as f64 * 0.01, v[2] as f64)),
+        &YELLOW,
+    ))
+    .unwrap();*/
+    chart
+        .draw_series(LineSeries::new(
+            rotations
+                .iter()
+                .enumerate()
+                .map(|(i, v)| (i as f64 * 0.01, v[0] as f64)),
+            &CYAN,
+        ))
+        .unwrap();
+    chart
+        .draw_series(LineSeries::new(
+            rotations
+                .iter()
+                .enumerate()
+                .map(|(i, v)| (i as f64 * 0.01, v[1] as f64)),
+            &YELLOW,
+        ))
+        .unwrap();
+    chart
+        .draw_series(LineSeries::new(
+            rotations
+                .iter()
+                .enumerate()
+                .map(|(i, v)| (i as f64 * 0.01, v[2] as f64)),
+            &BLACK,
+        ))
+        .unwrap();
+
+    println!("test");
 }
--- src/physics/integrator.rs
+++ src/physics/integrator.rs
@@ -15,20 +15,49 @@ impl Integrator {
 	}
 
 	pub fn step(&self, body: &mut Rigidbody, delta: f32) {
-		body.position = body.position + delta * body.velocity + 0.5 * self.gravity * delta * delta;
-		body.velocity = body.velocity + delta * self.gravity;
+		// Apply gravity
+		//body.position = body.position + delta * body.velocity + 0.5 * self.gravity * delta * delta;
+		//body.velocity = body.velocity + delta * self.gravity;
 
+		// Apply motors
 		let mut inverse_inertia: Matrix3<f32> = Matrix3::zeros();
 		na::linalg::try_invert_to(body.inertia, &mut inverse_inertia);
 
+		//let force_pos: Vector3<f32> = Vector3::new(1.0, 0.0, 0.0);
 		let force_pos: Vector3<f32> = Vector3::new(1.0, 0.0, 0.0);
 		let force: Vector3<f32> = Vector3::new(0.0, 1.0, 0.0);
 
-		//println!("{:?}", inverse_inertia);
+		let angular_acceleration = inverse_inertia * force_pos.cross(&force);
+
+		// Estimate position and velocity
+		let half_delta = delta / 2.0;
+		let half_rotation: Vector3<f32> = 0.5 * half_delta * half_delta * angular_acceleration
+			+ half_delta * body.angular_velocity
+			+ body.rotation;
+		let half_theta: f32 = half_rotation.norm();
+		let half_e: Vector3<f32> = half_rotation.normalize();
+		let half_acceleration: Vector3<f32>;
+		if half_theta == 0.0 {
+			half_acceleration = (1.0 * body.mass) * force; // + self.gravity;
+		} else {
+			half_acceleration = (1.0 * body.mass)
+				* (half_theta.cos() * force
+					+ half_theta.sin() * half_e.cross(&force)
+					+ (1.0 - half_theta.cos()) * (half_e.dot(&force)) * half_e);
+			//+ self.gravity;
+		}
+		let half_velocity = body.velocity + half_delta * half_acceleration;
+
+		body.velocity = body.velocity + delta * half_acceleration;
+		body.position = body.position + delta * half_velocity;
+
+		// Rotation and rotational velocity can be determined exactly
 		body.rotation = body.rotation
 			+ delta * body.angular_velocity
-			+ 0.5 * delta * delta * inverse_inertia * force_pos.cross(&force);
-		body.angular_velocity =
-			body.angular_velocity + delta * inverse_inertia * force_pos.cross(&force);
+			+ 0.5 * delta * delta * angular_acceleration;
+		body.angular_velocity = body.angular_velocity + delta * angular_acceleration;
+
+		body.rotation =
+			(body.rotation.norm() % (std::f32::consts::PI * 2.0)) * body.rotation.normalize();
 	}
 }
